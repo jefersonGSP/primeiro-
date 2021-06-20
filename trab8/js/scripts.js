cff7245c367b9dd8809b590cb409b5735f7a4295
jefersonGSP <71088768+jefersonGSP@users.noreply.github.com>
2021-06-20 16:12:45 -0300

    Add files via upload

---
+++ trab8/js/scripts.js
@@ -0,0 +1,65 @@
+function compraRealizada() {
+
+    var texto = document.getElementById('compra');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+    texto.style.fontSize = "1.2rem";
+    texto.style.fontWeight = "600";
+    document.getElementById('compra').innerText = "Compra realizada com sucesso!";
+
+}
+
+function adicionadoCar() {
+
+    var texto = document.getElementById('carinho');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+    texto.style.fontSize = "1.2rem";
+    texto.style.fontWeight = "600";
+    document.getElementById('carinho').innerText = "Adicionado";
+
+}
+
+function adcCarinho() {
+    var texto = document.getElementById('card');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+}
+
+function adcCarinho2() {
+    var texto = document.getElementById('card2');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+}
+
+function adcCarinho3() {
+    var texto = document.getElementById('card3');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+}
+
+function adcCarinho4() {
+    var texto = document.getElementById('card4');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+}
+
+function adcCarinho5() {
+    var texto = document.getElementById('card5');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+}
+
+function adcCarinho6() {
+    var texto = document.getElementById('card6');
+
+    texto.style.color = "green";
+    texto.style.backgroundColor = "rgb(152, 253, 152)";
+}
